71cbe2110a36e6a10197a67678a2a34c5e559737
fletch8 <stampsfj04@gmail.com>
2021-02-19 18:51:04 -0500

    deploy: attemp 5

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 
-config = require(__dirname + '/../config/production.json')[env];
+const config = require(__dirname + '/../config/production.json')[env];
 
 const db = {};
 
